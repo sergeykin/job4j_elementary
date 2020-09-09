1c2210b1ef6ede664692bc7409ff666fe943dbfc
sega <sergeykinn@mail.ru>
2020-09-09 21:08:12 +0300

    Разбор первой программы. [#359643]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sergey Kinyapin");
+        System.out.println("22.06.1979");
+    }
+}
